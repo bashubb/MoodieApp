029c5825824a097f6ded84fa1ef87e25fb1b3b99
Hubert Kiełkowski <hubert.kielkowski@gmail.com>
2026-02-11 20:38:20 +0100

    Initial commit

---
+++ Moodie/MoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodieApp.swift
+//  Moodie
+//
+//  Created by Hubert Private on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
